e7f11af4619ac05c1c0a7fce8d012bea9d2ce348
bparra26 <brayanpp2697@gmail.com>
2025-03-07 14:59:23 -0500

    agrego clases de utilidad para controlar los mensajes de error de las apis, generar y decodificar token jwt y para crear un usuario por defecto para la creacion de token

---
+++ src/main/java/com/dvp/challenge/infrastructure/utils/AuthDataUtil.java
@@ -0,0 +1,9 @@
+package com.dvp.challenge.infrastructure.utils;
+
+import java.util.UUID;
+
+public class AuthDataUtil {
+   public static final String ID = UUID.randomUUID().toString();
+   public static final String USERNAME = "cparra@gmail.com";
+   public static final String PASSWORD = "1122Q!";
+}
