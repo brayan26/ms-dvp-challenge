3799f597c13dc0d00f04af143416c357380b0c1b
bparra26 <brayanpp2697@gmail.com>
2025-03-07 15:03:28 -0500

    agrego clases de implementacion para la inversion de dependencia de mis repositorios de dominio

---
+++ src/main/java/com/dvp/challenge/infrastructure/impl/TicketRepositoryImpl.java
@@ -0,0 +1,73 @@
+package com.dvp.challenge.infrastructure.impl;
+
+import com.dvp.challenge.domain.Ticket;
+import com.dvp.challenge.infrastructure.errors.TicketError;
+import com.dvp.challenge.domain.repositories.ITicketRepository;
+import com.dvp.challenge.infrastructure.exceptions.GenericNotFoundException;
+import com.dvp.challenge.infrastructure.mapper.TicketMapper;
+import com.dvp.challenge.infrastructure.orm.entities.TicketEntity;
+import com.dvp.challenge.infrastructure.orm.repositories.JpaTicketRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class TicketRepositoryImpl implements ITicketRepository {
+   private final JpaTicketRepository jpaTicketRepository;
+
+   @Override
+   @Transactional
+   public Ticket create(Ticket ticket) {
+      TicketEntity entity = TicketMapper.toEntity(ticket);
+      return TicketMapper.toDomain(this.jpaTicketRepository.save(entity));
+   }
+
+   @Override
+   @Transactional
+   public Ticket edit(String id, Ticket ticket) {
+      Optional<TicketEntity> entity = this.jpaTicketRepository.findById(id);
+      if (entity.isEmpty()) {
+         throw new GenericNotFoundException(
+               String.format("<TicketRepositoryImpl.edit> ticket id %s not fount in the database", id),
+               TicketError.create().notFound());
+      }
+      TicketEntity toSave = TicketMapper.toEntity(ticket);
+      toSave.setId(id);
+      toSave.setCreatedAt(entity.get().getCreatedAt());
+      return TicketMapper.toDomain(this.jpaTicketRepository.save(toSave));
+   }
+
+   @Override
+   @Transactional
+   public void delete(String id) {
+      this.jpaTicketRepository.deleteById(id);
+   }
+
+   @Override
+   @Transactional(readOnly = true)
+   public Ticket findById(String id) {
+      Optional<TicketEntity> entity = this.jpaTicketRepository.findById(id);
+      if (entity.isEmpty()) {
+         throw new GenericNotFoundException(
+               String.format("<TicketRepositoryImpl.findById> ticket id %s not found in the database", id),
+               TicketError.create().notFound());
+      }
+      return TicketMapper.toDomain(entity.get());
+   }
+
+   @Override
+   @Transactional(readOnly = true)
+   public List<Ticket> find(int page, int size) {
+      Pageable pageable = PageRequest.of(page, size);
+      return this.jpaTicketRepository.findAll(pageable)
+            .stream()
+            .map(TicketMapper::toDomain)
+            .toList();
+   }
+}
---
+++ src/main/java/com/dvp/challenge/infrastructure/impl/UserRepositoryImpl.java
@@ -0,0 +1,92 @@
+package com.dvp.challenge.infrastructure.impl;
+
+import com.dvp.challenge.domain.AccessTokenSerializer;
+import com.dvp.challenge.domain.User;
+import com.dvp.challenge.infrastructure.errors.UsersError;
+import com.dvp.challenge.domain.repositories.IUserRepository;
+import com.dvp.challenge.infrastructure.exceptions.GenericBadRequestException;
+import com.dvp.challenge.infrastructure.exceptions.GenericNotFoundException;
+import com.dvp.challenge.infrastructure.mapper.UserMapper;
+import com.dvp.challenge.infrastructure.orm.entities.AuthEntity;
+import com.dvp.challenge.infrastructure.orm.entities.UserEntity;
+import com.dvp.challenge.infrastructure.orm.repositories.JpaAuthRepository;
+import com.dvp.challenge.infrastructure.orm.repositories.JpaUserRepository;
+import com.dvp.challenge.infrastructure.utils.JwtUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class UserRepositoryImpl implements IUserRepository {
+   private final JpaUserRepository jpaUserRepository;
+   private final JpaAuthRepository jpaAuthRepository;
+   private final PasswordEncoder encoder;
+   private final JwtUtils jwtUtils;
+
+   @Override
+   @Transactional
+   public User create(User user) {
+      UserEntity saved = this.jpaUserRepository.save(UserMapper.toEntity(user));
+      return UserMapper.toDomain(saved);
+   }
+
+   @Override
+   @Transactional
+   public User update(String id, User user) {
+      Optional<UserEntity> entity = jpaUserRepository.findById(id);
+      if (entity.isEmpty()) {
+         throw new GenericNotFoundException(
+               String.format("<UserRepositoryImpl.findUserById> userId %s, not found in database", id),
+               UsersError.create().notFound());
+      }
+      UserEntity toSave = UserMapper.toEntity(user);
+      toSave.setId(id);
+      toSave.setCreatedAt(entity.get().getCreatedAt());
+      return UserMapper.toDomain(this.jpaUserRepository.save(toSave));
+   }
+
+   @Override
+   @Transactional(readOnly = true)
+   public List<User> find() {
+      return this.jpaUserRepository.findAll().stream()
+            .map(UserMapper::toDomain)
+            .collect(Collectors.toList());
+   }
+
+   @Override
+   @Transactional(readOnly = true)
+   public User findUserById(String id) {
+      Optional<UserEntity> entity = this.jpaUserRepository.findById(id);
+      if (entity.isEmpty()) {
+         throw new GenericNotFoundException(
+               String.format("<UserRepositoryImpl.findUserById> userId %s, not found in database", id),
+               UsersError.create().notFound());
+      }
+      return UserMapper.toDomain(entity.get());
+   }
+
+   @Override
+   @Transactional(readOnly = true)
+   public AccessTokenSerializer doLogin(String username, String password) {
+      Optional<AuthEntity> entity = this.jpaAuthRepository.validateUsername(username);
+      if (entity.isEmpty()) {
+         throw new GenericNotFoundException(
+               String.format("<UserRepositoryImpl.doLogin> username '%s' not found in database", username),
+               UsersError.create().invalidUser().build()
+         );
+      }
+      if (!encoder.matches(password, entity.get().getPassword())) {
+         throw new GenericBadRequestException(
+               String.format("<UserRepositoryImpl.doLogin> password for the username '%s' doesn't match", username),
+               UsersError.create().invalidUser().build()
+         );
+      }
+      return new AccessTokenSerializer(jwtUtils.generateJwtToken(username));
+   }
+}
